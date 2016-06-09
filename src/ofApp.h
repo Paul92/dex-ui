f3e2886155ce20c27d07c09950134ee14b1de646
Paul92 <paul.gafton@gmail.com>
2016-06-10 02:12:58 +0300

    Using the new background module in the application and fixed identation in ofApp module

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
-#include "layout.h"
+#include "background.h"
 #include "left.h"
 #include "right.h"
 #include "term.h"
@@ -11,29 +11,29 @@
 #define Y_OFFSET 0
 
 class ofApp : public ofBaseApp{
-public:
-  void setup();
-  void update();
-  void draw();
-  void keyPressed(int key);
-  
-  void keyReleased(int key);
-  void mouseMoved(int x, int y );
-  void mouseDragged(int x, int y, int button);
-  void mousePressed(int x, int y, int button);
-  void mouseReleased(int x, int y, int button);
-  void windowResized(int w, int h);
-  void dragEvent(ofDragInfo dragInfo);
-  void gotMessage(ofMessage msg);
+    public:
+        void setup();
+        void update();
+        void draw();
+        void keyPressed(int key);
 
-//  bool isRecording;
-//  bool isDrawing;
-//  int recordFrameCounter;
-//  int recordMaxFrames;
-//  
-//  Layout layout;
-//  Left left;
-//  Right right;
-//  Term term;
-//  Keyboard keyboard;
+        void keyReleased(int key);
+        void mouseMoved(int x, int y );
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        void windowResized(int w, int h);
+        void dragEvent(ofDragInfo dragInfo);
+        void gotMessage(ofMessage msg);
+
+        bool isRecording;
+        bool isDrawing;
+        int recordFrameCounter;
+        int recordMaxFrames;
+
+        Background background;
+        //  Left left;
+        //  Right right;
+        //  Term term;
+        //  Keyboard keyboard;
 };
