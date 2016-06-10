e5552eb4f5278e79a98a72562ce90d87a9e7459b
Paul92 <paul.gafton@gmail.com>
2016-06-10 22:02:07 +0300

    Commented out the left module. Stabilized.

--- src/left.h
+++ src/left.h
@@ -1,11 +1,11 @@
 /*
- 
- left.h
- 
- Left panel of UI utils.
- Manages timeDisplay, boxVisualization, graphs, and two header objects.
- 
- */
+
+   left.h
+
+   Left panel of UI utils.
+   Manages timeDisplay, boxVisualization, graphs, and two header objects.
+
+*/
 #pragma once
 #include "ofMain.h"
 #include "header.h"
@@ -16,29 +16,29 @@
 #include "animated.h"
 
 class Left : public Animated {
-  
-public:
-  Left();
-  void draw();
-  void setPos(float x_, float y_);
-  
-  float x;
-  float y;
-  float w;
-  float h;
-  
-  void updateDependencyEvents();
-  void updateDependencyDelays(int delay_);
-private:
-  Header header;
-  TimeDisplay timeDisplay;
-  BoxVisualization boxVis;
-  Graph g1;
-  Graph g2;
-  Graph g3;
-  Graph g4;
-  NoiseVisualization noiseVis;
-  Header footer;
-  
-  float footer_len;
+
+    public:
+        Left();
+        void draw();
+        void setPos(float x_, float y_);
+
+        float x;
+        float y;
+        float w;
+        float h;
+
+        void updateDependencyEvents();
+        void updateDependencyDelays(int delay_);
+    private:
+//        Header header;
+//        TimeDisplay timeDisplay;
+//        BoxVisualization boxVis;
+//        Graph g1;
+//        Graph g2;
+//        Graph g3;
+//        Graph g4;
+//        NoiseVisualization noiseVis;
+//        Header footer;
+//
+//        float footer_len;
 };
