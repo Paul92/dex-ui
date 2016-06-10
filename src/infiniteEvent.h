589762a378c17da755ecbf8bd3f4b76644a67654
Paul92 <paul.gafton@gmail.com>
2016-06-10 22:39:32 +0300

    Added the infiniteEvent exception class.

---
+++ src/infiniteEvent.h
@@ -0,0 +1,9 @@
+/**
+ * @class InfiniteEvent
+ *
+ * @brief Class to represent an exception caused by an infinite event.
+ *
+ * Does not store details about the event.
+ */
+class InfiniteEvent {
+};
