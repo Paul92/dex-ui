5a238f8ebaca729aafcdcdb0aea0d63706fa4d8b
Paul92 <paul.gafton@gmail.com>
2016-06-11 18:41:26 +0300

    Refactored text (ofFont wrapper) to contain the string.

--- src/text.cpp
+++ src/text.cpp
@@ -1,19 +1,28 @@
 #include "text.h"
 
-Text::Text() {
-  
-}
-
 void Text::setFont(string font, int size) {
-  ofFont.loadFont(font, size, true, true);
+    ofFont.loadFont(font, size, true, true);
 }
 
-void Text::drawString(string s, float x, float y) {
-  ofRectangle bounds = ofFont.getStringBoundingBox(s, x, y);
-  ofFont.drawString(s, x, y+bounds.height);
+Text::Text(std::string text, string font, int size) {
+    this->text = text;
+    setFont(font, size);
 }
 
-void Text::drawStringFromTopRight(string s, float x, float y) {
-  ofRectangle bounds = ofFont.getStringBoundingBox(s, x, y);
-  ofFont.drawString(s, x-bounds.width, y+bounds.height);
+void Text::drawString(ofPoint position) {
+    ofFont.drawString(text, position.x, position.y + ofFont.stringHeight(text));
+}
+
+void Text::drawStringFromTopRight(ofPoint position) {
+    ofFont.drawString(text,
+                      position.x - ofFont.stringWidth(text),
+                      position.y + ofFont.stringHeight(text));
+}
+
+int Text::getWidth() {
+    return ofFont.stringWidth(text);
+}
+
+int Text::getHeight() {
+    return ofFont.stringHeight(text);
 }
--- src/text.h
+++ src/text.h
@@ -1,26 +1,28 @@
-/*
- 
- text.h
- 
- Wrapper class for ofTrueTypeFont with more helpful utils
- 
- */
-
 #pragma once
+
 #include "ofMain.h"
 
+/**
+ * @class Text
+ *
+ * @brief Wrapper for ofTrueTypeFont, with a string member.
+ */
 class Text {
-  
-public:
-  Text();
-  
-  // Adjusted to draw from top left
-  void drawString(string s, float x, float y);
-  // Adjusted to draw from top right
-  void drawStringFromTopRight(string s, float x, float y);
-  
-  void setFont(string font, int size);
-  ofTrueTypeFont ofFont;
-  
-private:
+
+    public:
+        Text(std::string text, string font, int size);
+
+        /// Adjusted to draw from top left
+        void drawString(ofPoint position);
+        /// Adjusted to draw from top right
+        void drawStringFromTopRight(ofPoint position);
+
+        int getWidth();
+        int getHeight();
+
+    private:
+        void setFont(string font, int size);
+
+        ofTrueTypeFont ofFont;
+        std::string text;
 };
